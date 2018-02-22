93fb2906b8b0d656148de2876623daa2c5942352
joonchul <chulchulhae1@gmail.com>
2018-02-22 17:21:42 +0900

    Branch test01: 메인페이지 클릭 이벤트 추가. 2018.02.22

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 // import { CustomHeader } from '../share/custom-header/custom-header'
 
@@ -9,8 +9,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  list_main_images: Array<{ title:string, img_url:string, comment:string }>;
 
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+    this.list_main_images = [
+      {
+        title: '관제센터',
+        comment: '관제센터: 준비중입니다.',
+        img_url: 'http://atop21.mir9.co.kr/wp-content/uploads/2017/11/2009.jpg'
+      },
+      {
+        title: '발권시스템',
+        comment: '발권시스템: 준비중입니다.',
+        img_url: 'http://atop21.mir9.co.kr/wp-content/uploads/2017/11/2014-1.jpg'
+      },
+      {
+        title: '교통정보시스템',
+        comment: '교통정보시스템: 준비중입니다.',
+        img_url: 'http://atop21.mir9.co.kr/wp-content/uploads/2017/11/2013-1.jpg'
+      },
+      {
+        title: '통합관제센터',
+        comment: '통합관제센터: 준비중입니다.',
+        img_url: 'http://atop21.mir9.co.kr/wp-content/uploads/2017/11/2013.png'
+      }
+    ];
+  }
+
+  clickMainCardImage(comment) {
+    let toast = this.toastCtrl.create({
+      message: comment,
+      duration: 1000
+    });
+    toast.present();
   }
 
 }
